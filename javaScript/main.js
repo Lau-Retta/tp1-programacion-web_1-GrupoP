4be1b4713715060c67d20a9c2d31b9b5c236bbb8
Lautaro Retta <lautaroretta@gmail.com>
2025-11-08 12:48:03 -0300

    refactor: refactoriza nombre de cursos, inicializador de mains.js y agrupa los estilos para la actividad de los botones en error.css

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,5 +1,11 @@
 import { User } from "./login/user.js";
 
-User.initUsers();
+const init = () => {
+    User.initUsers();
+    const btnIncriptionIA = document.querySelector(".cabecera__btn");
+    btnIncriptionIA.addEventListener("click", () => {
+        window.location.href = "./pages/inscripcionIndividual/inscripcionIndividual.html?curso=curso-ia"
+    })
+}
 
-
+init();
